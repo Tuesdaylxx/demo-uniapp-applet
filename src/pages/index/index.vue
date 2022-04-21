cfd528786e9ea158eb895384517fa72d8b5e64de
白佑白 <2537140811@qq.com>
2022-04-22 00:57:58 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="content"> hello world </view>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/profile/profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="profile"> 个人中心 </view>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {},
+};
+</script>
+
+<style>
+</style>
